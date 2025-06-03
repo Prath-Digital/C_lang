d20b0754469bde007c34a4258a659ee66f2a2933
Prath Udhnawala <askforvarn@gmail.com>
2025-06-03 14:24:59 +0530

    Chapter 6

--- ch_6/Projects/q=3_6.3.c
+++ ch_6/Projects/q=3_6.3.c
@@ -7,7 +7,7 @@ int main()
     printf("Enter any number: ");
     scanf("%d", &N);
     
-    printf("Enter any highest multiplier: ");
+    printf("Enter any a multiplier: ");
     scanf("%d", &M);
     
     for (i = 1; i <= M; i++)
