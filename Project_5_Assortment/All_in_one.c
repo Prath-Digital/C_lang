642c22111bdce139ec258c09b2da4b2a63c47a86
Prath Udhnawala <askforvarn@gmail.com>
2025-06-06 12:07:51 +0530

    Project 5 Assortment

--- Project_5_Assortment/All_in_one.c
+++ Project_5_Assortment/All_in_one.c
@@ -7,7 +7,8 @@ int main()
         printf("Choose array type:\n");
         printf("1. 1D Array\n");
         printf("2. 2D Array\n");
-        printf("Enter your choice (1-2, 0 to exit): ");
+        printf("0. Exit\n");
+        printf("Enter your choice: ");
         scanf("%d", &choice);
 
         if (choice == 1)
@@ -24,28 +25,34 @@ int main()
                 scanf("%d", &a[i]);
             }
 
-            printf("Menu:\n");
-            printf("1. Find negative elements in 1D array\n");
-            printf("Enter your choice (1): ");
-            scanf("%d", &choice);
+            do {
+                printf("Menu:\n");
+                printf("1. Find negative elements in 1D array\n");
+                printf("0. Exit\n");
+                printf("Enter your choice: ");
+                scanf("%d", &choice);
 
-            switch (choice)
-            {
-            case 1:
-                printf("Negative elements from the Array: ");
-                for (i = 0; i < size; i++)
+                switch (choice)
                 {
-                    if (a[i] < 0)
+                case 1:
+                    printf("Negative elements from the Array: ");
+                    for (i = 0; i < size; i++)
                     {
-                        printf("%d ", a[i]);
+                        if (a[i] < 0)
+                        {
+                            printf("%d ", a[i]);
+                        }
                     }
-                }
-                printf("\n");
-                break;
+                    printf("\n");
+                    break;
 
-            default:
-                printf("Invalid choice.\n");
-            }
+                case 0:
+                    break;
+
+                default:
+                    printf("Invalid choice.\n");
+                }
+            } while (choice != 0);
         }
         else if (choice == 2)
         {
@@ -66,94 +73,100 @@ int main()
                 }
             }
 
-            printf("Menu:\n");
-            printf("1. Find largest element in 2D array\n");
-            printf("2. Find transpose of a 2D array\n");
-            printf("3. Print and sum a specific row & column from 2D array\n");
-            printf("Enter your choice (1-3): ");
-            scanf("%d", &choice);
+            do {
+                printf("Menu:\n");
+                printf("1. Find largest element in 2D array\n");
+                printf("2. Find transpose of a 2D array\n");
+                printf("3. Print and sum a specific row & column from 2D array\n");
+                printf("0. Exit\n");
+                printf("Enter your choice: ");
+                scanf("%d", &choice);
 
-            switch (choice)
-            {
-            case 1:
-            {
-                int max = a[0][0];
-                for (i = 0; i < row; i++)
+                switch (choice)
                 {
-                    for (j = 0; j < col; j++)
-                    {
-                        if (a[i][j] > max)
-                        {
-                            max = a[i][j];
-                        }
-                    }
-                }
-
-                printf("The largest element is: %d\n", max);
-                break;
-            }
-
-            case 2:
-            {
-                int transpose[col][row];
-                for (i = 0; i < row; i++) {
-                    for (j = 0; j < col; j++) {
-                        transpose[j][i] = a[i][j];
-                    }
-                }
-
-                printf("The transpose matrix of the array:\n");
-                for (i = 0; i < col; i++) {
-                    for (j = 0; j < row; j++) {
-                        printf("%d ", transpose[i][j]);
-                    }
-                    printf("\n");
-                }
-                break;
-            }
-
-            case 3:
-            {
-                int r, c, rowSum = 0, colSum = 0;
-                printf("Enter row number (0 to %d): ", row - 1);
-                scanf("%d", &r);
-                if (r >= 0 && r < row)
+                case 1:
                 {
-                    printf("Elements of row %d: ", r);
-                    for (j = 0; j < col; j++)
-                    {
-                        printf("%d ", a[r][j]);
-                        rowSum += a[r][j];
-                    }
-                    printf("\nThe sum of row %d: %d\n", r, rowSum);
-                }
-                else
-                {
-                    printf("Invalid row number.\n");
-                }
-
-                printf("Enter column number (0 to %d): ", col - 1);
-                scanf("%d", &c);
-                if (c >= 0 && c < col)
-                {
-                    printf("Elements of column %d: ", c);
+                    int max = a[0][0];
                     for (i = 0; i < row; i++)
                     {
-                        printf("%d ", a[i][c]);
-                        colSum += a[i][c];
+                        for (j = 0; j < col; j++)
+                        {
+                            if (a[i][j] > max)
+                            {
+                                max = a[i][j];
+                            }
+                        }
                     }
-                    printf("\nThe sum of column %d: %d\n", c, colSum);
-                }
-                else
-                {
-                    printf("Invalid column number.\n");
-                }
-                break;
-            }
 
-            default:
-                printf("Invalid choice.\n");
-            }
+                    printf("The largest element is: %d\n", max);
+                    break;
+                }
+
+                case 2:
+                {
+                    int transpose[col][row];
+                    for (i = 0; i < row; i++) {
+                        for (j = 0; j < col; j++) {
+                            transpose[j][i] = a[i][j];
+                        }
+                    }
+
+                    printf("The transpose matrix of the array:\n");
+                    for (i = 0; i < col; i++) {
+                        for (j = 0; j < row; j++) {
+                            printf("%d ", transpose[i][j]);
+                        }
+                        printf("\n");
+                    }
+                    break;
+                }
+
+                case 3:
+                {
+                    int r, c, rowSum = 0, colSum = 0;
+                    printf("Enter row number (0 to %d): ", row - 1);
+                    scanf("%d", &r);
+                    if (r >= 0 && r < row)
+                    {
+                        printf("Elements of row %d: ", r);
+                        for (j = 0; j < col; j++)
+                        {
+                            printf("%d ", a[r][j]);
+                            rowSum += a[r][j];
+                        }
+                        printf("\nThe sum of row %d: %d\n", r, rowSum);
+                    }
+                    else
+                    {
+                        printf("Invalid row number.\n");
+                    }
+
+                    printf("Enter column number (0 to %d): ", col - 1);
+                    scanf("%d", &c);
+                    if (c >= 0 && c < col)
+                    {
+                        printf("Elements of column %d: ", c);
+                        for (i = 0; i < row; i++)
+                        {
+                            printf("%d ", a[i][c]);
+                            colSum += a[i][c];
+                        }
+                        printf("\nThe sum of column %d: %d\n", c, colSum);
+                    }
+                    else
+                    {
+                        printf("Invalid column number.\n");
+                    }
+                    break;
+                }
+
+                case 0:
+                    break;
+
+                default:
+                    printf("Invalid choice.\n");
+                }
+            } while (choice != 0);
         }
         else if (choice != 0)
         {
